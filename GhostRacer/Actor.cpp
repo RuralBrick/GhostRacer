467602d350e9aba0b0cf5991b24492efe8c3c7ee
Teddy1405 <48705917+Teddy1405@users.noreply.github.com>
2021-02-14 01:32:00 -0800

    some ghostracer and borderline

--- GhostRacer/Actor.cpp
+++ GhostRacer/Actor.cpp
@@ -2,3 +2,11 @@
 #include "StudentWorld.h"
 
 // Students:  Add code to this file, Actor.h, StudentWorld.h, and StudentWorld.cpp
+
+void BorderLine::doSomething() {
+	
+}
+
+WhiteBorderLine::WhiteBorderLine(double x, double y) : BorderLine(x, y) {
+	lastWhiteBorderLine = this;
+}
--- GhostRacer/Actor.h
+++ GhostRacer/Actor.h
@@ -5,4 +5,44 @@
 
 // Students:  Add code to this file, Actor.cpp, StudentWorld.h, and StudentWorld.cpp
 
+class Actor : public GraphObject {
+public:
+	Actor();
+	virtual void doSomething() = 0;
+	bool alive() {
+		return m_alive;
+	}
+private:
+	bool m_alive;
+};
+
+class GhostRacer : public Actor {
+public:
+	GhostRacer(double x, double y);
+	virtual void doSomething();
+};
+
+class WhiteBorderLine;
+
+class BorderLine : public Actor {
+public:
+	BorderLine(double x, double y);
+	virtual void doSomething();
+	static double lastWhiteBorderLineY() {
+		return lastWhiteBorderLine->getY();
+	}
+protected:
+	static WhiteBorderLine* lastWhiteBorderLine;
+};
+
+class YellowBorderLine : public BorderLine {
+public:
+	YellowBorderLine(double x, double y);
+};
+
+class WhiteBorderLine : public BorderLine {
+public:
+	WhiteBorderLine(double x, double y);
+};
+
 #endif // ACTOR_H_
